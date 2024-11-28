7608c2c460f908795a2ca7f74501ae2444ac9b1d
DiegoBlancoMos <dblancom23@fpcoruna.afundacion.org>
2024-11-28 10:04:36 +0100

    backend añadido.

--- Proyecto_Juego/scripts/muerte.gd
+++ Proyecto_Juego/scripts/muerte.gd
@@ -3,8 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	send_post_new_score()
-	send_get_request()
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
@@ -18,21 +17,3 @@ func _on_volverjugar_pressed() -> void:
 func _on_salir_pressed() -> void:
 	get_tree().quit()
 	
-func send_post_new_score():
-	var http_request = HTTPRequest.new()
-	add_child(http_request)
-	var body = JSON.stringify({"username": "Personaje", "score": Global.count_globos})
-	var headers = ["Content-Type: application/json", "Client-Secret: abc"] # CLIENT_SECRET should never be public! If leaked, ALL clients should be force-updated to use a new one
-	http_request.request("http://127.0.0.1:8000/score", headers, HTTPClient.METHOD_POST, body)
-
-func send_get_request():
-	var http_request = HTTPRequest.new()
-	add_child(http_request)
-	http_request.connect("request_completed", _on_server_responded)
-	var headers = ["Content-Type: application/json", "Client-Secret: abc"] # CLIENT_SECRET should never be public! If leaked, ALL clients should be force-updated to use a new one
-	http_request.request("http://127.0.0.1:8000/score", headers, HTTPClient.METHOD_GET)
-
-func _on_server_responded(result, response_code, headers, body):
-	var response = JSON.parse_string(body.get_string_from_utf8())
-	print("Server response:")
-	print(response)
